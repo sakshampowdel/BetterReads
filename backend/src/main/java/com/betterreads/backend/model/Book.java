8d4a1f15e5a05932b8784205e0510b832d44e643
Saksham Powdel <sakshampowdel@gmail.com>
2025-05-15 17:42:59 -0400

    chore(backend): add book entity

---
+++ backend/src/main/java/com/betterreads/backend/model/Book.java
@@ -0,0 +1,40 @@
+package com.betterreads.backend.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Book {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String title;
+    private String author;
+    private String isbn;
+
+    protected Book() {}
+
+    public Book(String title, String author, String isbn) {
+        this.title = title;
+        this.author = author;
+        this.isbn = isbn;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+}
