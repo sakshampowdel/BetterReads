fdf295c9a41979b4ef19daf73cbcefe9b39b7bac
Saksham Powdel <sakshampowdel@gmail.com>
2025-05-15 18:01:37 -0400

    chore(backend): add and extend bookrepository interface

---
+++ backend/src/main/java/com/betterreads/backend/repository/BookRepository.java
@@ -0,0 +1,9 @@
+package com.betterreads.backend.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.betterreads.backend.model.Book;
+
+public interface BookRepository extends JpaRepository<Book, Long>{
+    
+}
