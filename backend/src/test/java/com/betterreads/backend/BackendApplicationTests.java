bc4adc479a5fe133eae1da4d026d70bd574cf69d
Saksham Powdel <sakshampowdel@gmail.com>
2025-05-15 15:55:06 -0400

    chore: initialize project structure with backend and frontend folders

---
+++ backend/src/test/java/com/betterreads/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.betterreads.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
