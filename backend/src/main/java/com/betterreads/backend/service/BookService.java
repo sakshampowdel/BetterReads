6f912b7fa6381e39f4e8e88cc9c53d12e62eb575
Saksham Powdel <sakshampowdel@gmail.com>
2025-05-16 19:20:50 -0400

    fix(backend): integrate booknotfound exception with getbookbyid

--- backend/src/main/java/com/betterreads/backend/service/BookService.java
+++ backend/src/main/java/com/betterreads/backend/service/BookService.java
@@ -1,25 +1,26 @@
 package com.betterreads.backend.service;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import java.util.Optional;
 import org.springframework.stereotype.Service;
 
 import com.betterreads.backend.repository.BookRepository;
+import com.betterreads.backend.exception.BookNotFoundException;
 import com.betterreads.backend.model.Book;
 
 @Service
 public class BookService {
     private final BookRepository bookRepository;
 
-    @Autowired
     public BookService(final BookRepository bookRepository) {
         this.bookRepository = bookRepository;
     }
 
     public Book getBookById(Long id) {
-        if (!bookRepository.existsById(id)) {
-            return null;
+        Optional<Book> book = bookRepository.findById(id);
+        if (book.isEmpty()) {
+            throw new BookNotFoundException("Book with id " + id + " doesn't exist!");
         }
 
-        return bookRepository.getReferenceById(id);
+        return book.get();
     }
 }
