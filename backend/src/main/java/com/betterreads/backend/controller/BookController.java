b6a4f57352505c67a21f7322b7ab14d7c2f2caa6
Saksham Powdel <sakshampowdel@gmail.com>
2025-05-16 19:19:33 -0400

    feat(backend): add get mapping for single book

--- backend/src/main/java/com/betterreads/backend/controller/BookController.java
+++ backend/src/main/java/com/betterreads/backend/controller/BookController.java
@@ -1,7 +1,9 @@
 package com.betterreads.backend.controller;
 
 import com.betterreads.backend.model.Book;
+import com.betterreads.backend.service.BookService;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,8 +12,15 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/books")
 public class BookController {
-    @GetMapping
-    public Book getBook(@PathVariable Long id) {
-        return new Book(null, null, null);
+    private final BookService bookService;
+
+    public BookController(BookService bookService) {
+        this.bookService = bookService;
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Book> getBook(@PathVariable Long id) {
+        Book book = bookService.getBookById(id);
+        return ResponseEntity.ok(book);
     }
 }
