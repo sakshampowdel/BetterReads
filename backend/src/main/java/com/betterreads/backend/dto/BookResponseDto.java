8a085c373e3b2575e7cf2645cbf38b8a14cc76a2
Saksham Powdel <sakshampowdel@gmail.com>
2025-05-16 23:55:59 -0400

    chore(backend): add new dto for responses

---
+++ backend/src/main/java/com/betterreads/backend/dto/BookResponseDto.java
@@ -0,0 +1,31 @@
+package com.betterreads.backend.dto;
+
+public class BookResponseDto {
+    private Long id;
+    private String title;
+    private String author;
+    private String isbn;
+
+    public BookResponseDto(Long id, String title, String author, String isbn) {
+        this.id = id;
+        this.title = title;
+        this.author = author;
+        this.isbn = isbn;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+}
