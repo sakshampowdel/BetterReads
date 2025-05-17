33bd3a293fa84b68b3e8986d474502daad4dca35
Saksham Powdel <sakshampowdel@gmail.com>
2025-05-17 16:19:40 -0400

    chore(backend): create authornotfound exception

---
+++ backend/src/main/java/com/betterreads/backend/exception/AuthorNotFoundException.java
@@ -0,0 +1,7 @@
+package com.betterreads.backend.exception;
+
+public class AuthorNotFoundException extends RuntimeException {
+    public AuthorNotFoundException(String message) {
+        super(message);
+    }
+}
