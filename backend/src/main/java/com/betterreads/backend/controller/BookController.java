1186e6e3611f8220c8d366f193faf7cfc6b56dfc
Saksham Powdel <sakshampowdel@gmail.com>
2025-05-16 22:17:25 -0400

    feat(backend): add updatebook and deletebook

--- backend/src/main/java/com/betterreads/backend/controller/BookController.java
+++ backend/src/main/java/com/betterreads/backend/controller/BookController.java
@@ -9,9 +9,11 @@ import jakarta.validation.Valid;
 import java.util.List;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -38,8 +40,23 @@ public class BookController {
     }
 
     @PostMapping()
-    public ResponseEntity<Book> createBook(@Valid @RequestBody BookRequestDto bookRequestDto) {
+    public ResponseEntity<Book> createBook(
+        @Valid @RequestBody BookRequestDto bookRequestDto) {
         Book book = bookService.createBook(bookRequestDto);
         return ResponseEntity.status(201).body(book);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Book> updateBook(
+        @PathVariable Long id,
+        @Valid @RequestBody BookRequestDto bookRequestDto) {
+        Book book = bookService.updateBookById(id, bookRequestDto);
+        return ResponseEntity.ok(book);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteBook(@PathVariable Long id) {
+        bookService.deleteBookById(id);
+        return ResponseEntity.noContent().build();
     }
 }
