e860c4b95e614815f8eaee42284f1264111ccb0a
Saksham Powdel <sakshampowdel@gmail.com>
2025-05-17 16:01:40 -0400

    fix(backend): add final to bookcontroller arg

--- backend/src/main/java/com/betterreads/backend/controller/BookController.java
+++ backend/src/main/java/com/betterreads/backend/controller/BookController.java
@@ -23,7 +23,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class BookController {
     private final BookService bookService;
 
-    public BookController(BookService bookService) {
+    public BookController(final BookService bookService) {
         this.bookService = bookService;
     }
 
