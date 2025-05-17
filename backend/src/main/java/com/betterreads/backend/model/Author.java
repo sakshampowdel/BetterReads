15969164d65abc8d8741694814e7ecb3ce654b38
Saksham Powdel <sakshampowdel@gmail.com>
2025-05-17 16:29:38 -0400

    feat(backend): add getter for id

--- backend/src/main/java/com/betterreads/backend/model/Author.java
+++ backend/src/main/java/com/betterreads/backend/model/Author.java
@@ -19,6 +19,10 @@ public class Author {
         this.openLibraryId = openLibraryId;
     }
 
+    public Long getId() {
+        return id;
+    }
+
     public String getName() {
         return name;
     }
