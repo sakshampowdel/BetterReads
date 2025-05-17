2a87eabe9b55af9437cbf4066a09102c6c7b2bc2
Saksham Powdel <sakshampowdel@gmail.com>
2025-05-17 18:24:10 -0400

    feat(backend): add paginated getauthors

--- backend/src/main/java/com/betterreads/backend/controller/AuthorController.java
+++ backend/src/main/java/com/betterreads/backend/controller/AuthorController.java
@@ -1,13 +1,22 @@
 package com.betterreads.backend.controller;
 
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.betterreads.backend.dto.AuthorRequestDto;
 import com.betterreads.backend.dto.AuthorResponseDto;
+import com.betterreads.backend.dto.PaginatedResponseDto;
 import com.betterreads.backend.service.AuthorService;
+
+import jakarta.validation.Valid;
 
 @RestController
 @RequestMapping("/authors")
@@ -22,5 +31,29 @@ public class AuthorController {
     public ResponseEntity<AuthorResponseDto> getAuthor(@PathVariable Long id) {
         AuthorResponseDto authorResponseDto = authorService.getAuthorById(id);
         return ResponseEntity.ok(authorResponseDto);
+    }
+
+    @GetMapping()
+    public ResponseEntity<PaginatedResponseDto<AuthorResponseDto>> getAuthors(Pageable pageable) {
+        PaginatedResponseDto<AuthorResponseDto> paginatedResponseDto = authorService.getAllAuthors(pageable);
+        return ResponseEntity.status(201).body(paginatedResponseDto);
+    }
+
+    @PostMapping()
+    public ResponseEntity<AuthorResponseDto> createBook(
+        @Valid @RequestBody AuthorRequestDto authorRequestDto) {
+        return null;
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<AuthorResponseDto> updateBook(
+        @PathVariable Long id,
+        @Valid @RequestBody AuthorRequestDto authorRequestDto) {
+        return null;
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteBook(@PathVariable Long id) {
+        return null;
     }
 }
