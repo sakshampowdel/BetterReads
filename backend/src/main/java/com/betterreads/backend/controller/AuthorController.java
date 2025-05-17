c4d80b4fea4c59c4d820edace702c49e36932104
Saksham Powdel <sakshampowdel@gmail.com>
2025-05-17 17:40:29 -0400

    feat(backend): add getauthor

--- backend/src/main/java/com/betterreads/backend/controller/AuthorController.java
+++ backend/src/main/java/com/betterreads/backend/controller/AuthorController.java
@@ -1,8 +1,12 @@
 package com.betterreads.backend.controller;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.betterreads.backend.dto.AuthorResponseDto;
 import com.betterreads.backend.service.AuthorService;
 
 @RestController
@@ -12,5 +16,11 @@ public class AuthorController {
 
     public AuthorController(final AuthorService authorService) {
         this.authorService = authorService;
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<AuthorResponseDto> getAuthor(@PathVariable Long id) {
+        AuthorResponseDto authorResponseDto = authorService.getAuthorById(id);
+        return ResponseEntity.ok(authorResponseDto);
     }
 }
