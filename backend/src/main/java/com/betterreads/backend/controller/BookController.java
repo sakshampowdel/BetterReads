a8de002abb0b0ba4aa7584237edabdf6c0a45258
Saksham Powdel <sakshampowdel@gmail.com>
2025-05-16 21:41:53 -0400

    feat(backend): add getbooks and createbook

--- backend/src/main/java/com/betterreads/backend/controller/BookController.java
+++ backend/src/main/java/com/betterreads/backend/controller/BookController.java
@@ -1,11 +1,18 @@
 package com.betterreads.backend.controller;
 
+import com.betterreads.backend.dto.BookRequestDto;
 import com.betterreads.backend.model.Book;
 import com.betterreads.backend.service.BookService;
+
+import jakarta.validation.Valid;
+
+import java.util.List;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -22,5 +29,17 @@ public class BookController {
     public ResponseEntity<Book> getBook(@PathVariable Long id) {
         Book book = bookService.getBookById(id);
         return ResponseEntity.ok(book);
+    }
+
+    @GetMapping()
+    public ResponseEntity<List<Book>> getBooks() {
+        List<Book> list = bookService.getAllBooks();
+        return ResponseEntity.ok(list);
+    }
+
+    @PostMapping()
+    public ResponseEntity<Book> createBook(@Valid @RequestBody BookRequestDto bookRequestDto) {
+        Book book = bookService.createBook(bookRequestDto);
+        return ResponseEntity.status(201).body(book);
     }
 }
