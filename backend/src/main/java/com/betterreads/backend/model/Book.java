c9bf08c8a5872270727a8bf2f8020dc76d0bfbf8
Saksham Powdel <sakshampowdel@gmail.com>
2025-05-16 22:16:59 -0400

    feat(backend): add setters for book

--- backend/src/main/java/com/betterreads/backend/model/Book.java
+++ backend/src/main/java/com/betterreads/backend/model/Book.java
@@ -37,4 +37,16 @@ public class Book {
     public String getIsbn() {
         return isbn;
     }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
 }
