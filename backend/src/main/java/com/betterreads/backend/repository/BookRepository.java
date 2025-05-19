ae9cb70b6ba5eecf4c41ad814940770ae11768d2
Saksham Powdel <sakshampowdel@gmail.com>
2025-05-19 18:50:27 -0400

    feat(backend): add findbyisbn

--- backend/src/main/java/com/betterreads/backend/repository/BookRepository.java
+++ backend/src/main/java/com/betterreads/backend/repository/BookRepository.java
@@ -1,9 +1,11 @@
 package com.betterreads.backend.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.betterreads.backend.model.Book;
 
 public interface BookRepository extends JpaRepository<Book, Long> {
-    
+    Optional<Book> findByIsbn(String isbn);
 }
