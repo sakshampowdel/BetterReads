f185af1353101bea6a0eb4672b35905086c90df8
Saksham Powdel <sakshampowdel@gmail.com>
2025-05-19 18:40:33 -0400

    feat(backend): add findbyopenlibraryid

--- backend/src/main/java/com/betterreads/backend/repository/AuthorRepository.java
+++ backend/src/main/java/com/betterreads/backend/repository/AuthorRepository.java
@@ -1,9 +1,11 @@
 package com.betterreads.backend.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.betterreads.backend.model.Author;
 
 public interface AuthorRepository extends JpaRepository<Author, Long> {
-    
+    Optional<Author> findByOpenLibraryId(String openLibraryId);
 }
