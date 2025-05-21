f016dddb8fa8d81d06fe8a45b56fe6ec913788b4
Saksham Powdel <sakshampowdel@gmail.com>
2025-05-20 23:41:01 -0400

    chore(backend): add tailwind css

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,11 @@
 import "./App.css";
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <div className="text-red-500 text-2xl font-bold">Hello Tailwind</div>
+    </>
+  );
 }
 
 export default App;
