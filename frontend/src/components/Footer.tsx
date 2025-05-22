8d2f2333226719e1a0cad33ed7e2a249f2f22110
Saksham Powdel <sakshampowdel@gmail.com>
2025-05-22 01:26:41 -0400

    chore(frontend): create basic footer

---
+++ frontend/src/components/Footer.tsx
@@ -0,0 +1,9 @@
+function Footer() {
+  return (
+    <div className="md:flex bg-white py-10 px-8 max-md:text-center justify-center">
+      <h1 className="p-3 font-bold">BetterReads</h1>
+    </div>
+  );
+}
+
+export default Footer;
