9248d4adc1b39cc54671faa4262ab13024904850
Saksham Powdel <sakshampowdel@gmail.com>
2025-05-21 23:33:35 -0400

    feat(frontend): add featured books section

--- frontend/src/pages/landing/LandingPage.tsx
+++ frontend/src/pages/landing/LandingPage.tsx
@@ -1,9 +1,11 @@
+import FeaturedBooks from "./FeaturedBooks";
 import HeroSection from "./HeroSection";
 
 function LandingPage() {
   return (
     <>
       <HeroSection />
+      <FeaturedBooks />
     </>
   );
 }
