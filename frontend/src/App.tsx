57d4704657521f9947eb0024c0be5f2f9cdab4ee
Saksham Powdel <sakshampowdel@gmail.com>
2025-05-21 22:56:09 -0400

    feat(frontend): add landing page to app

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,12 @@
 import "./App.css";
 import Navbar from "./components/Navbar.tsx";
+import LandingPage from "./pages/landing/LandingPage.tsx";
 
 function App() {
   return (
     <>
       <Navbar />
+      <LandingPage />
     </>
   );
 }
