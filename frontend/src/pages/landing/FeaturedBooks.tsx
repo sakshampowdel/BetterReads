92d1188b635bcafb8789fae845cff9b326d2a8e5
Saksham Powdel <sakshampowdel@gmail.com>
2025-05-21 23:34:05 -0400

    chore(frontend): create featured books section

---
+++ frontend/src/pages/landing/FeaturedBooks.tsx
@@ -0,0 +1,10 @@
+function FeaturedBooks() {
+  return (
+    <div className="md:flex-column bg-white py-10 px-8 max-md:text-center space-y-8">
+      <h1 className="font-semibold text-3xl">Featured Books</h1>
+      <div></div>
+    </div>
+  );
+}
+
+export default FeaturedBooks;
