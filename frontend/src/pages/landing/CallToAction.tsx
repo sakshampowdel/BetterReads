1f860f77773656cbaf5608bf386171cb8448cb0d
Saksham Powdel <sakshampowdel@gmail.com>
2025-05-22 01:27:21 -0400

    chore(frontend): create calltoaction section of landing page

---
+++ frontend/src/pages/landing/CallToAction.tsx
@@ -0,0 +1,12 @@
+function CallToAction() {
+  return (
+    <div className="md:flex bg-[#F7F7F7] py-10 px-8 max-md:text-center justify-evenly gap-4 space-y-4 min-h-40 items-center">
+      <h1 className="text-3xl font-bold">Join the community!</h1>
+      <button className="bg-black text-white py-3 px-5 rounded-md max-h-12">
+        Sign Up
+      </button>
+    </div>
+  );
+}
+
+export default CallToAction;
