9ed367bb60969ed8f9eae5b45dea7983ae131fd9
Saksham Powdel <sakshampowdel@gmail.com>
2025-05-26 21:39:05 -0400

    feat(frontend): add routing for pages

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,15 +1,20 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
 import Footer from "./components/Footer.tsx";
 import Navbar from "./components/Navbar.tsx";
 import LandingPage from "./pages/landing/LandingPage.tsx";
+import BrowsePage from "./pages/browse/BrowsePage.tsx";
 
 function App() {
   return (
-    <>
+    <BrowserRouter>
       <Navbar />
-      <LandingPage />
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/browse" element={<BrowsePage />} />
+      </Routes>
       <Footer />
-    </>
+    </BrowserRouter>
   );
 }
 
