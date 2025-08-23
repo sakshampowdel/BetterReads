104df6423b722d9881b34bdc222fbca89009b110
Saksham Powdel <sakshampowdel@gmail.com>
2025-08-22 20:50:50 -0400

    feat(frontend): add trending books section

--- frontend/src/pages/landing/Landing.tsx
+++ frontend/src/pages/landing/Landing.tsx
@@ -1,9 +1,11 @@
 import Hero from "./Hero";
+import TrendingBooks from "./TrendingBooks";
 
 const Landing = () => {
   return (
     <div>
       <Hero />
+      <TrendingBooks />
     </div>
   );
 };
