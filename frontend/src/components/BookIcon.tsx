17d8a40ae4d89279014f8d0d26dd34507ce38dc1
Saksham Powdel <sakshampowdel@gmail.com>
2025-10-06 22:36:29 -0400

    fix(frontend): change link from book to work cover and work on adding default cover

--- frontend/src/components/BookIcon.tsx
+++ frontend/src/components/BookIcon.tsx
@@ -1,13 +1,15 @@
 import type { Book } from "../types/Book";
 
 const BookIcon = ({ book }: { book: Book }) => {
-  const imgLink = `https://covers.openlibrary.org/b/OLID/${book.openlibraryid}-M.jpg`;
+  const imgLink = `https://covers.openlibrary.org/w/olid/${book.openLibraryId}-M.jpg?default=false`;
 
   return (
-    <div className="w-full">
+    <div className="w-full aspect-[2/3] overflow-hidden rounded-xl bg-neutral-100 flex items-center justify-center text-accent-foreground text-center">
       <img
         src={imgLink}
-        className="w-full h-auto outline-2 rounded-xl outline-accent-foreground"
+        alt={book.title}
+        onError={(e) => (e.currentTarget.src = "")}
+        className="w-full h-full object-cover"
       />
     </div>
   );
