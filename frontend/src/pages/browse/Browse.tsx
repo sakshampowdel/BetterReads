281ccb2b5834894937757f531064ba737f46d38c
Saksham Powdel <sakshampowdel@gmail.com>
2025-10-06 22:39:09 -0400

    feat(frontend): use new api imports and add loading for book icons

--- frontend/src/pages/browse/Browse.tsx
+++ frontend/src/pages/browse/Browse.tsx
@@ -1,54 +1,39 @@
 import { useEffect, useState } from "react";
 import type { Book } from "../../types/Book";
-import type { Paginated } from "../../types/Paginated";
-
-async function fetchBooks(
-  page: number,
-  size: number
-): Promise<Paginated<Book>> {
-  const res = await fetch(`/api/books?page=${page}&size=${size}`);
-  if (!res.ok) throw new Error("Failed to fetch books");
-  return res.json();
-}
+import { fetchBooks } from "../../api";
+import BookIcon from "../../components/BookIcon";
 
 const Browse = () => {
   const [books, setBooks] = useState<Book[]>([]);
+  const [loading, setLoading] = useState(true);
   const [page, setPage] = useState(0);
-  const [totalPages, setTotalPages] = useState(0);
+  const PAGE_SIZE = 10;
 
   useEffect(() => {
-    fetchBooks(page, 5).then((data) => {
-      setBooks(data.data);
-      setTotalPages(data.totalPages);
-    });
+    fetchBooks(page, PAGE_SIZE)
+      .then((data) => {
+        setBooks(data.data);
+        console.log("Books resposne:", data.data);
+      })
+      .catch((err) => console.error(err))
+      .finally(() => setLoading(false));
   }, [page]);
 
   return (
-    <div>
-      <h2>Books</h2>
-      <ul>
-        {books.map((book) => (
-          <li key={book.id}>
-            <strong>{book.title}</strong> —{" "}
-            {book.authors.map((a) => a.name).join(", ")}
-            <p>{book.description}</p>
-          </li>
-        ))}
-      </ul>
-
-      <button disabled={page === 0} onClick={() => setPage((p) => p - 1)}>
-        Prev
-      </button>
-      <span>
-        {page + 1} / {totalPages}
-      </span>
-      <button
-        disabled={page + 1 >= totalPages}
-        onClick={() => setPage((p) => p + 1)}
-      >
-        Next
-      </button>
-    </div>
+    <section id="trending" className="p-24 px-48 max-md:p-2">
+      <h1 className="text-4xl font-semibold max-md:text-2xl">Trending Books</h1>
+      <div className="py-8 grid grid-cols-2 sm:grid-cols-4  lg:grid-cols-10 gap-2">
+        {loading ? (
+          <div>Loading...</div>
+        ) : (
+          <div>
+            {books.map((book) => (
+              <BookIcon key={book.id} book={book} />
+            ))}
+          </div>
+        )}
+      </div>
+    </section>
   );
 };
 
