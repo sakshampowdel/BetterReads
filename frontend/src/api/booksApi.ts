1149407844482401caea66e1a94a0fe321b2b794
Saksham Powdel <sakshampowdel@gmail.com>
2025-10-09 21:37:48 -0400

    chore(frontend): add title query to filter out pageable responses

--- frontend/src/api/booksApi.ts
+++ frontend/src/api/booksApi.ts
@@ -3,9 +3,12 @@ import type { Paginated } from "../types/Paginated";
 
 export async function fetchBooks(
   page: number,
-  size: number
+  size: number,
+  title: string
 ): Promise<Paginated<Book>> {
-  const res = await fetch(`/api/books?page=${page}&size=${size}`);
+  const res = await fetch(
+    `/api/books?page=${page}&size=${size}&title=${encodeURIComponent(title)}`
+  );
   if (!res.ok) throw new Error("Failed to fetch books");
   return res.json();
 }
