7de18185cc0b2935991bcfa0ec30f02925bc8253
Saksham Powdel <sakshampowdel@gmail.com>
2025-10-11 19:54:56 -0400

    chore(frontend): create 404 Not Found page for router

---
+++ frontend/src/pages/errors/NotFound.tsx
@@ -0,0 +1,11 @@
+const NotFound = () => {
+  return (
+    <main className="min-h-screen pt-12 px-48 max-md:p-2 space-y-4">
+      <div className="text-center text-5xl">
+        Sorry, looks like this page doesn't exist! :(
+      </div>
+    </main>
+  );
+};
+
+export default NotFound;
