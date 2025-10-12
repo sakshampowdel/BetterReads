8d916f8a989ebd4397f9d8abd5da53d153679cd6
Saksham Powdel <sakshampowdel@gmail.com>
2025-10-11 21:41:41 -0400

    feat(frontend): add styling and handle markdown formatted book descriptions

--- frontend/src/pages/book/BookDetails.tsx
+++ frontend/src/pages/book/BookDetails.tsx
@@ -2,6 +2,8 @@ import { fetchBookById } from "../../api";
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import type { Book } from "../../types/Book";
+import BookPicture from "../../components/BookPicture";
+import ReactMarkdown from "react-markdown";
 
 function checkId({ id }: { id: string | undefined }) {
   if (id === undefined) return -1;
@@ -25,23 +27,32 @@ const BookDetails = () => {
       .catch((err) => console.log(err));
   }, [id]);
 
+  if (!book) {
+    return (
+      <main className="min-h-screen flex items-center justify-center">
+        <h1 className="text-4xl font-bold">Book Not Found!</h1>
+      </main>
+    );
+  }
+
   return (
-    <main>
-      {book === null ? (
-        <div>
-          <h1>Book Not Found!</h1>
-        </div>
-      ) : (
-        <div>
-          <h1>{book.title}</h1>
+    <main className="min-h-screen p-6 md:px-24 lg:px-48 space-y-8">
+      <div className="max-w-6xl mx-auto flex flex-col md:flex-row gap-8 items-center md:items-start">
+        <BookPicture book={book} />
+        <div className="flex-1 space-y-4">
+          <h1 className="text-5xl font-bold underline break-words text-center md:text-left">
+            {book.title}
+          </h1>
           <div>
             {book.authors.map((author) => (
-              <h2>{author.name}</h2>
+              <h2 key={author.name} className="text-2xl font-light">
+                {author.name}
+              </h2>
             ))}
           </div>
-          <p>{book.description}</p>
+          <ReactMarkdown>{book.description}</ReactMarkdown>
         </div>
-      )}
+      </div>
     </main>
   );
 };
