0d71155f2bc40488673a426275f44dd5de5c769e
Saksham Powdel <sakshampowdel@gmail.com>
2025-10-14 18:17:59 -0400

    test(backend): add jwt service test

---
+++ backend/src/test/java/com/betterreads/backend/service/JwtServiceTest.java
@@ -0,0 +1,28 @@
+package com.betterreads.backend.service;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import com.betterreads.backend.model.User;
+
+@SpringBootTest
+public class JwtServiceTest {
+
+  @Autowired
+  private JwtService jwtService;
+
+  @Test
+  void testGenerateAndValidateToken() {
+    User user = new User("testuser", "test@example.com", "password");
+    String token = jwtService.generateToken(user);
+
+    assertNotNull(token);
+    assertTrue(jwtService.isTokenValid(token, user));
+    assertEquals("test@example.com", jwtService.extractEmail(token));
+  }
+}
