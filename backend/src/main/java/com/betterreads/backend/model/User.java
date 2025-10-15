d13f5c04101857ef5a0c72a0cb000b8055b7e4eb
Saksham Powdel <sakshampowdel@gmail.com>
2025-10-14 20:19:48 -0400

    feat(backend): implement UserDetails and their methods, change username to displayname and make unique

--- backend/src/main/java/com/betterreads/backend/model/User.java
+++ backend/src/main/java/com/betterreads/backend/model/User.java
@@ -1,5 +1,12 @@
 package com.betterreads.backend.model;
 
+import java.util.Collection;
+import java.util.Collections;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -8,20 +15,21 @@ import jakarta.persistence.Table;
 
 @Entity
 @Table(name = "users")
-public class User {
+public class User implements UserDetails {
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Long id;
 
-  private String username;
+  @Column(unique = true, nullable = false)
+  private String displayName;
   private String email;
   private String password;
 
   protected User() {
   }
 
-  public User(String username, String email, String password) {
-    this.username = username;
+  public User(String displayName, String email, String password) {
+    this.displayName = displayName;
     this.email = email;
     this.password = password;
   }
@@ -30,27 +38,59 @@ public class User {
     return id;
   }
 
-  public String getUsername() {
-    return username;
+  public String getDisplayName() {
+    return displayName;
   }
 
   public String getEmail() {
     return email;
   }
 
+  @Override
   public String getPassword() {
     return password;
   }
 
-  public void setUsername(String username) {
-    this.username = username;
+  public void setDisplayName(String displayName) {
+    this.displayName = displayName;
   }
 
   public void setEmail(String email) {
     this.email = email;
   }
 
-  public void setPasswordHash(String password) {
+  public void setPasswordValue(String password) {
     this.password = password;
   }
+
+  @Override
+  public Collection<? extends GrantedAuthority> getAuthorities() {
+    return Collections.emptyList();
+  }
+
+  @Override
+  public String getUsername() {
+    return email;
+  }
+
+  @Override
+  public boolean isAccountNonExpired() {
+    return true;
+  }
+
+  @Override
+  public boolean isAccountNonLocked() {
+    return true;
+  }
+
+  @Override
+  public boolean isCredentialsNonExpired() {
+    return true;
+  }
+
+  @Override
+  public boolean isEnabled() {
+    return true;
+  }
+
 }
