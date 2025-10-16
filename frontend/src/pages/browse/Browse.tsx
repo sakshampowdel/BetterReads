371219f46005231220cc0ccf6b4c2f0c7266ca54
Saksham Powdel <sakshampowdel@gmail.com>
2025-10-16 00:16:22 -0400

    style(frontend): change input search box to consistent style

--- frontend/src/pages/browse/Browse.tsx
+++ frontend/src/pages/browse/Browse.tsx
@@ -5,9 +5,9 @@ const Browse = () => {
   const [titleQuery, setTitleQuery] = useState("");
 
   return (
-    <main className="min-h-screen pt-12 px-48 max-md:p-2 space-y-4">
+    <main className="min-h-[calc(100vh-7rem)] pt-12 px-48 max-md:p-2 space-y-4">
       <input
-        className="text-2xl py-1 min-w-full border-2 border-muted rounded-sm"
+        className="text-2xl py-1 px-2 min-w-full border-2 border-muted rounded-sm bg-background focus:outline-none focus:border-accent transition-colors"
         type="search"
         id="browse-books"
         placeholder="Search..."
