aca7d8eb554568cb7fa213fc67dad7f44b89cdd5
Saksham Powdel <sakshampowdel@gmail.com>
2025-10-17 12:15:40 -0400

    feat(backend): create new profile entity

---
+++ backend/src/main/java/com/betterreads/backend/model/Profile.java
@@ -0,0 +1,75 @@
+package com.betterreads.backend.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.OneToOne;
+
+@Entity
+public class Profile {
+  @Id
+  @GeneratedValue
+  private Long id;
+  private String displayName;
+  private String bio;
+  @OneToOne
+  @JoinColumn(name = "user_id", nullable = false)
+  private User user;
+  @OneToMany
+  private List<BookList> bookLists = new ArrayList<>();
+
+  protected Profile() {
+  }
+
+  public Profile(String displayName, String bio, User user) {
+    this.displayName = displayName;
+    this.bio = bio;
+    this.user = user;
+  }
+
+  public Long getId() {
+    return id;
+  }
+
+  public String getDisplayName() {
+    return displayName;
+  }
+
+  public String getBio() {
+    return bio;
+  }
+
+  public User getUser() {
+    return user;
+  }
+
+  public List<BookList> getBookLists() {
+    return bookLists;
+  }
+
+  public void setDisplayName(String displayName) {
+    this.displayName = displayName;
+  }
+
+  public void setBio(String bio) {
+    this.bio = bio;
+  }
+
+  public void setUser(User user) {
+    this.user = user;
+  }
+
+  public void addBookList(BookList booklist) {
+    bookLists.add(booklist);
+  }
+
+  public void removeBookList(BookList booklist) {
+    bookLists.remove(booklist);
+  }
+
+}
