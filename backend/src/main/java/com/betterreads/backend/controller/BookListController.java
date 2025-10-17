2e7229799aa4fe7ce9b27133dce6f259c90288cc
Saksham Powdel <sakshampowdel@gmail.com>
2025-10-17 16:17:24 -0400

    feat(backend): create booklist controller

---
+++ backend/src/main/java/com/betterreads/backend/controller/BookListController.java
@@ -0,0 +1,48 @@
+package com.betterreads.backend.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.betterreads.backend.dto.BookListRequestDto;
+import com.betterreads.backend.dto.BookListResponseDto;
+import com.betterreads.backend.model.User;
+import com.betterreads.backend.service.BookListService;
+
+@RestController
+@RequestMapping("/api/booklists")
+public class BookListController {
+  private final BookListService bookListService;
+
+  public BookListController(BookListService bookListService) {
+    this.bookListService = bookListService;
+  }
+
+  @GetMapping("/{id}")
+  public ResponseEntity<BookListResponseDto> getBookList(@PathVariable Long id) {
+    BookListResponseDto response = bookListService.getBookListById(id);
+    return ResponseEntity.ok(response);
+  }
+
+  @PostMapping
+  public ResponseEntity<BookListResponseDto> createBookList(@RequestBody BookListRequestDto bookListRequestDto,
+      Authentication authentication) {
+    User user = (User) authentication.getPrincipal();
+    BookListResponseDto bookListResponseDto = bookListService.createBookList(user, bookListRequestDto.getName());
+    return ResponseEntity.status(201).body(bookListResponseDto);
+  }
+
+  @DeleteMapping("/{id}")
+  public ResponseEntity<Void> deleteBookList(@PathVariable Long id, Authentication authentication) {
+    User user = (User) authentication.getPrincipal();
+    bookListService.deleteBookList(id, user);
+    return ResponseEntity.noContent().build();
+  }
+
+}
