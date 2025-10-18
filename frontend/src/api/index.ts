8ddfb6bfa7850000f8ccf44407a688f38fc201b6
Saksham Powdel <sakshampowdel@gmail.com>
2025-10-18 15:32:00 -0400

    feat(frontend): add booklist and reviews api

--- frontend/src/api/index.ts
+++ frontend/src/api/index.ts
@@ -2,3 +2,5 @@ export * from "./booksApi";
 export * from "./authorsApi";
 export * from "./usersApi";
 export * from "./profilesApi";
+export * from "./bookListsApi";
+export * from "./reviewsApi";
